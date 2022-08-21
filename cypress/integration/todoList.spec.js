9347a8736a1c6b5b1e22ba5a802261ae921a70e4
Anicka Cercado <anicka.cercado@jobsity.io>
2022-08-21 00:18:06 -0600

    Adding todolist spec

---
+++ cypress/integration/todoList.spec.js
@@ -0,0 +1,30 @@
+/// <reference types="cypress" />
+
+describe("TODO list page", () => {
+  beforeEach(() => {
+    cy.visit("https://example.cypress.io/todo");
+  });
+
+  it("Validate URL", () => {
+    cy.url().should("include", "/todo");
+  });
+
+  it("Add two items ", () => {
+    cy.get("[data-test=new-todo]").type("Learn JavaScript{enter}");
+    cy.get("[data-test=new-todo]").type("Learn Cypress{enter}");
+  });
+
+  it("Check and item as completed & see completed items ", () => {
+    cy.contains("Pay electric bill")
+      .parent()
+      .find("input[type=checkbox]")
+      .check();
+
+    cy.contains("Completed").click();
+  });
+
+  it("Go to Utilities", () => {
+    cy.contains("Utilities").click();
+    cy.url().should("include", "/utilities");
+  });
+});
